dac304173465da41be31fc399cabe2ba09fe23da
Amir Tarkian <111789603+amirtarkian@users.noreply.github.com>
2026-02-26 19:23:02 -0600

    Delete src/CAL/migrations directory

--- src/CAL/migrations/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Migration utilities for CAL."""
-
-from .migrate_legacy_memory import migrate_legacy_memory_json, migrate_legacy_memory_payload
-
-__all__ = [
-    "migrate_legacy_memory_json",
-    "migrate_legacy_memory_payload",
-]
